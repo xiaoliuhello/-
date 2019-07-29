cfb33eada6805c752720f441c77d0b6f1a8e6770
xiaoliuhello <995301275@qq.com>
2019-07-29 14:02:22 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-fo="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in iconList" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+            <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票景点门票景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '北京必游'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+        desc: '古北水镇'
+      }, {
+        id: '008',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/0334cf5430b9b5505fd79e2b8d7e8670.png',
+        desc: '爬长城'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+        desc: '水上乐园'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+@import "~styles/mixins.styl"
+.icons >>> .swiper-container{
+  height 0
+  padding-bottom 50%;
+}
+.icons{
+  overflow hidden;
+  height 0;
+  padding-bottom 50%;
+}
+
+.icon {
+  overflow hidden;
+  width:25%;
+  height 0
+  float left ;
+  padding-bottom 25%;
+  position relative;
+}
+  .icon-img{
+    position absolute;
+    top 0;
+    left 0;
+    right 0;
+    bottom .44rem;
+    box-sizing: border-box;
+    padding .1rem;
+  }
+  .icon-img-content{
+    display block;
+    margin 0 auto;
+    height:100%
+  }
+  .icon-desc{
+    position absolute;
+    left 0;
+    right 0;
+    bottom 0;
+    height .44rem;
+    line-height .44rem;
+    text-align center;
+    color: $darkTextColor;
+    ellipsis()
+  }
+</style>
