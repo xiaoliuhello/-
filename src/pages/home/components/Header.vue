034d035f7110e3d53adb49fc63a063f6dfb878eb
xiaoliuhello <995301275@qq.com>
2019-07-29 23:38:02 +0800

    router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,16 +2,18 @@
   <div>
       <div class="header">
         <div class="header-left">
-          <span class="iconfont">&#xe624;</span>
+          <div class="iconfont back-icon">&#xe624;</div>
         </div>
         <div class="header-input">
           <span class="iconfont">&#xe632;</span>
           输入城市/景点/游玩
         </div>
-        <div class="header-right">
-          {{this.city}}
-          <span class="iconfont arrow-iconfont">&#xea81;</span>
-        </div>
+        <router-link to="/city">
+          <div class="header-right">
+            {{this.city}}
+            <span class="iconfont arrow-iconfont">&#xea81;</span>
+          </div>
+        </router-link>
       </div>
   </div>
 </template>
@@ -30,7 +32,7 @@ export default {
 @import "~styles/varibles.styl"
   .header{
     display: flex ;
-    line-height:.86rem;
+    line-height: $headerHeight;
     background :$bgColor;
     color #fff;
   }
@@ -56,6 +58,7 @@ export default {
     width :1.24rem;
     float :right ;
     text-align :center;
+    color #fff;
   }
   .arrow-iconfont{
     font-size .24rem;
